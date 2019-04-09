121304ac2897defa4c26ae4a4a874110a4b1bfba
MikeBarskiy <mikebarskiy@gmail.com>
2019-04-09 17:22:04 +0300

    Training of acquired skills. Adding implementation of mkdir command

--- src/demo_filesystem/commands/Mkdir.scala
+++ src/demo_filesystem/commands/Mkdir.scala
@@ -1,6 +1,6 @@
 package demo_filesystem.commands
 
-import demo_filesystem.files.Directory
+import demo_filesystem.files.{DirEntry, Directory}
 import demo_filesystem.filesystem.State
 
 class Mkdir(name: String) extends Command {
@@ -23,6 +23,40 @@ class Mkdir(name: String) extends Command {
   }
 
   def doMkdir(state: State, name: String): State = {
-    ???
+    def updateStructure(currentDirectory: Directory, path: List[String], newEntry: DirEntry): Directory = {
+      if (path.isEmpty) currentDirectory.addEntry(newEntry)
+      else {
+        val oldEntry = currentDirectory.findEntry(path.head).asDirectory
+        currentDirectory.replaceEntry(oldEntry.name, updateStructure(oldEntry, path.tail, newEntry))
+        /*
+          /a/b
+            /c
+            /d
+            (new Entry)
+          currentDir = /a
+          path = ["b"]
+         */
+      }
+      /*
+        someDir  /a /b (new) /c => new someDir /a /b /c
+        /a/b/  /c  /d  (new) /e => new /a  new /b (parent /a)
+       */
+    }
+
+    val wd = state.wd
+
+    // 1. All the directories in the full path
+    val allDirsInPath = wd.getAllFoldersInPath()
+
+    // 2. Create new directory in the the working directory(wd)
+    val newDirectory = Directory.empty(wd.path, name)
+
+    // 3. Update the whole directory structure starting from the root (the directory structure is IMMUTABLE)
+    val newRoot = updateStructure(state.root, allDirsInPath, newDirectory)
+
+    // 4. Find new working directory instance given wd's full path in the new directory structure
+    val newWd = newRoot.findDescedant(allDirsInPath)
+
+    State(newRoot, wd)
   }
 }

7c7667c4c3eb961fcff17522c359d1b7cbee6c0b
MikeBarskiy <mikebarskiy@gmail.com>
2019-04-10 23:51:04 +0300

    Starting to understand how to work with higher-order functions.

---
+++ src/lectures/part3fp/HOFsAndCurries.scala
@@ -0,0 +1,19 @@
+package lectures.part3fp
+
+object HOFsAndCurries extends App {
+
+  // : receive function or return other function as result
+
+  // function that applies a function n times over a value x
+  // nTimes(f, n, x)
+  // nTimes(f, 3, x) = f(f(f(x))) = nTimes(f, 2, f(x))
+
+  def nTimes(f: Int => Int, n: Int, x: Int): Int = {
+    if (n <= 0) x
+    else nTimes(f, n - 1, f(x))
+  }
+
+  val plusOne = (x: Int) => x + 1
+  println(nTimes(plusOne, 10, 1)) // 11 (1+1+1..+1)
+}
+

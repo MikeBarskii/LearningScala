2edc919827eb89b4f60a585c178ca469fcf6b627
MikeBarskiy <mikebarskiy@gmail.com>
2019-04-11 00:18:27 +0300

    Trying to understand how to work with higher-order functions. Pass functions as parameters, get them as a result. Was tried curried functions - function with multiple parameter lists

--- src/lectures/part3fp/HOFsAndCurries.scala
+++ src/lectures/part3fp/HOFsAndCurries.scala
@@ -14,6 +14,29 @@ object HOFsAndCurries extends App {
   }
 
   val plusOne = (x: Int) => x + 1
-  println(nTimes(plusOne, 10, 1)) // 11 (1+1+1..+1)
+  println(nTimes(plusOne, 10, 1)) // 11 (1 + 1 +...+ 1)
+
+  //
+  def nTimesBetter(f: Int => Int, n: Int): (Int => Int) = {
+    if (n <= 0) (x: Int) => x
+    else (x: Int) => nTimesBetter(f, n - 1)(f(x))
+  }
+
+  val plus10 = nTimesBetter(plusOne, 10)
+  println(plus10(1)) // 11 (1 + 1 +...+ 1)
+
+  // curried function
+  val superAdder: Int => (Int => Int) = (x: Int) => (y: Int) => x + y
+  val add3 = superAdder(3) // y => 3 + y
+  println(add3(10)) // 13
+
+  // functions with multiple parameter lists
+  def curriedFormatter(c: String)(x: Double): String = c.format(x)
+
+  val standardFormat: Double => String = curriedFormatter("%4.2f")
+  val preciseFormat: Double => String = curriedFormatter("%10.8f")
+
+  println(standardFormat(Math.PI)) // 3,14
+  println(preciseFormat(Math.PI)) // 3,14159265
 }
 

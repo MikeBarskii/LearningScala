c5f90a6e14ad6191561de6cc14fb2437846325d2
MikeBarskiy <mikebarskiy@gmail.com>
2019-04-11 16:56:38 +0300

    Final result of demo project. "Echo" and "cat" command were added. Some improvements were included.

--- src/demo_filesystem/files/File.scala
+++ src/demo_filesystem/files/File.scala
@@ -2,7 +2,7 @@ package demo_filesystem.files
 
 import demo_filesystem.filesystem.FilesystemException
 
-class File(override val parentPath: String, override val name: String, contents: String) extends DirEntry(parentPath, name) {
+class File(override val parentPath: String, override val name: String, val contents: String) extends DirEntry(parentPath, name) {
 
   override def asDirectory: Directory = throw new FilesystemException("A file can not be converted to a directory!")
 
@@ -13,6 +13,12 @@ class File(override val parentPath: String, override val name: String, contents:
   override def isDirectory: Boolean = false
 
   override def isFile: Boolean = true
+
+  def setContents(newContents: String): File =
+    new File(parentPath, name, newContents)
+
+  def appendContents(newContents: String): File =
+    setContents(contents + "\n" + newContents)
 }
 
 object File {
